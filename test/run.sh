8e17697fb29329463d5e3f78968f32c7e27cea83
Soichi Hayashi <hayashis@iu.edu>
2014-02-11 18:28:11 +0000

    some updates to test script

--- test/run.sh
+++ test/run.sh
@@ -4,9 +4,9 @@ rundir=/tmp/$RANDOM
 echo "using rundir:$rundir"
 mkdir $rundir
 
-#./setup2.py hayashis IU-GALAXY nr latest /home/iugalaxy/test/samples/nr.5000 blastx '-evalue 0.001' $rundir
+(cd .. && ./setup.py hayashis IU-GALAXY nr latest /home/hayashis/git/osg-blast/sample/nr.5000 blastx '-evalue 0.001' $rundir)
 #./setup2.py hayashis IU-GALAXY nr latest /home/iugalaxy/test/samples/nr.38142 blastx '-evalue 0.001' $rundir
-../setup.py hayashis IU-GALAXY nr latest /home/iugalaxy/test/samples/nr.70937 blastx '-evalue 0.001' $rundir
+#../setup.py hayashis IU-GALAXY nr latest /home/iugalaxy/test/samples/nr.70937 blastx '-evalue 0.001' $rundir
 
 #cd $rundir
 #condor_submit_dag blast.dag
